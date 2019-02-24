c28c22ed2273b000aa0c1aaaedc64a86d81623b8
Jesus Moreno <moreno.jesus2212@gmail.com>
2019-02-24 16:03:14 -0400

    Preparations for Titanium Bow

---
+++ src/main/java/fieryphoenix99/mod/objects/items/tools/ToolBow.java
@@ -0,0 +1,13 @@
+package fieryphoenix99.mod.objects.items.tools;
+
+import fieryphoenix99.mod.util.interfaces.IHasModel;
+import net.minecraft.item.ItemBow;
+
+public class ToolBow extends ItemBow implements IHasModel {
+
+	public ToolBow(String string, ToolMaterial toolMaterialTitanium) {}
+
+	@Override
+	public void registerModels() {}
+
+}
